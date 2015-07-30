87d40a593a087e14854bfbf524a936eae37352b3
shamseen <shamseen.c@gmail.com>
2015-07-30 19:06:18 -0400

    trying to change the Help page.. didn't work.

--- Lab8_WebAPI/Controllers/BooksController.cs
+++ Lab8_WebAPI/Controllers/BooksController.cs
@@ -115,5 +115,22 @@ namespace Lab8_WebAPI.Controllers
         {
             return db.Books.Count(e => e.Id == id) > 0;
         }
+
+        /// <summary>
+        /// Gets some very important data from the server.
+        /// </summary>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Looks up some data by ID.
+        /// </summary>
+        /// <param name="id">The ID of the data.</param>
+        public string Get(int id)
+        {
+            return "value";
+        }
     }
 }
